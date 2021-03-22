ddd7f8844e7360818673ef237677b9d7d2be6d0f
PetrTech <55279432+PetrTech@users.noreply.github.com>
2021-03-22 17:23:21 +0100

    Update Main.java

--- Xopitele/Main.java
+++ Xopitele/Main.java
@@ -1,5 +1,5 @@
 public class main {
-    public static void main(String[] args){
-        System.out.println("Running");
+    public static void Main(String[] args){
+        System.out.println("Xopitele Engine Initialized");
     }
 }
