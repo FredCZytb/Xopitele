43490c40d786002bd077491f4f4ddf40d5587baa
PetrTech <55279432+PetrTech@users.noreply.github.com>
2021-03-22 17:35:51 +0100

    Updated main class

--- Xopitele/Main.java
+++ Xopitele/Main.java
@@ -1,5 +1,12 @@
-public class main {
-    public static void Main(String[] args){
-        System.out.println("Xopitele Engine Initialized");
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+    public static void main(String[] args){
+        System.out.println("Running");
+        JFrame gui = new JFRame("Xopitele - Running Application");
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        gui.pack();
+        gui.setVisible(true);
     }
 }
